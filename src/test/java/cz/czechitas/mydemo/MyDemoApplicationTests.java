23adea86b103ab2551d0f450e5aa7cb9a994a4a2
mvdegtyar <mv_degtyar@yahoo.com>
2022-12-12 19:52:18 +0200

    Initial commit

---
+++ src/test/java/cz/czechitas/mydemo/MyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.mydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
